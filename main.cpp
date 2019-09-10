9b2fb0890afec95c00176dec4ea8ec0809eb6f7d
smz19 <sergio.197@hotmail.com>
2019-09-10 02:05:43 -0600

    Configuración inicial

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
+#include <QApplication>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+int main(int argc, char *argv[]) {
+
+    QApplication app(argc, argv);
+
+
+
+    return app.exec();
 }
